ad917252447cdf64baf238cbe965d8200161bf5d
Shane <mussershane@gmail.com>
2018-01-09 19:19:55 -0500

    Format change for main PC

--- AsciiSharp/AsciiSharp/Game1.cs
+++ AsciiSharp/AsciiSharp/Game1.cs
@@ -69,7 +69,7 @@ namespace AsciiSharp {
             graphics.PreferredBackBufferWidth = width;  // set this value to the desired width of your window
             graphics.PreferredBackBufferHeight = height;   // set this value to the desired height of your window
             graphics.ApplyChanges();
-            Buffer.AsciiBuffer.Update();
+            //Buffer.AsciiBuffer.Update();
             
         }
     }
